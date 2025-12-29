43891df3951b524067f8d0e263142881b8e987f5
Adam Thomas <thomasadam83@hotmail.com>
2025-12-28 19:21:55 -0500

    Update user_fetch function

--- functions/user.php
+++ functions/user.php
@@ -15,34 +15,47 @@ function user_name($id)
 function user_fetch($identifier, $field = false)
 {
 
-    if(ENV_SSO == false)
+    if(ENV_SSO)
     {
-        return $_SESSION['user'];
+        
+        global $connect;
+
+        if($field)
+        {
+            $query = 'SELECT *
+                FROM users
+                WHERE '.$field.' = "'.addslashes($identifier).'"
+                LIMIT 1';
+        }
+        else
+        {
+            $query = 'SELECT *
+                FROM users
+                WHERE id = "'.addslashes($identifier).'"
+                OR email = "'.addslashes($identifier).'"
+                OR github_username = "'.addslashes($identifier).'"
+                OR (reset_hash = "'.addslashes($identifier).'" AND reset_hash != "")
+                OR (verify_hash = "'.addslashes($identifier).'" AND verify_hash != "")
+                LIMIT 1';
+        }
+
+        $result = mysqli_query($connect, $query);
+
+        if(mysqli_num_rows($result)) return mysqli_fetch_assoc($result);
+        else return false;
+
+    }
+    elseif(is_numeric($identifier))
+    {
+        
+        $url = ENV_SSO_DOMAIN.'/api/user/'.$identifier.'<br>';
+
+        $data = fetch_json($url);
+
+        return $data['user'];
+
     }
 
-    global $connect;
-
-    if($field)
-    {
-        $query = 'SELECT *
-            FROM users
-            WHERE '.$field.' = "'.addslashes($identifier).'"
-            LIMIT 1';
-    }
-    else
-    {
-        $query = 'SELECT *
-            FROM users
-            WHERE id = "'.addslashes($identifier).'"
-            OR email = "'.addslashes($identifier).'"
-            OR github_username = "'.addslashes($identifier).'"
-            OR (reset_hash = "'.addslashes($identifier).'" AND reset_hash != "")
-            OR (verify_hash = "'.addslashes($identifier).'" AND verify_hash != "")
-            LIMIT 1';
-    }
-    $result = mysqli_query($connect, $query);
-
-    if(mysqli_num_rows($result)) return mysqli_fetch_assoc($result);
-    else return false;
+    return false;
 
 }
